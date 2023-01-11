be733b772264361f9a331cd376a433a94fe71593
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-01-11 21:58:29 +0530

    Update pipeline.py

--- huggingfaceinference/pipeline.py
+++ huggingfaceinference/pipeline.py
@@ -3,4 +3,24 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 gtokenizer = AutoTokenizer.from_pretrained("vishnun/tinygram")
 gmodel = AutoModelForSeq2SeqLM.from_pretrained("vishnun/tinygram")
 
-def gramcorrector(gtokenizer, gmodel):
+def gramcorrector(gtokenizer, gmodel, n=None):
+  
+  if n is None:
+    n == 1
+  text = input()
+  input_ids = gtokenizer.encode(text, return_tensors='pt')
+  outputs = gmodel.generate(
+    input_ids,
+    do_sample=True, 
+    max_length=50,
+    top_p=0.999,
+    top_k=45,
+    num_return_sequences=n
+  )
+  
+  out_text = []
+  for y in outputs:
+    out_text.append(gtokenizer.decode(y, skip_special_tokens=True))
+  
+  return out_text
+    
